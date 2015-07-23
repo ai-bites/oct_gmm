88ff39dd7c53fa6a5a6e7fb2c88208283c54b374
kalpanki <vasan.shrini@gmail.com>
2015-07-23 19:30:38 +0200

    adding possibility to do texture features using LBP

--- preprocess.m
+++ preprocess.m
@@ -1,4 +1,4 @@
-function [X,flat_vols] = preprocess(location,num_vols, dataset)
+function [X,flat_vols] = preprocess(location,num_vols, dataset, method)
 % Does the preprocessing on the mat files in the location.
 %
 % location - folder+filename. Eg, 'data/DME/patient' where 
@@ -14,28 +14,75 @@ function [X,flat_vols] = preprocess(location,num_vols, dataset)
         volumes{i} = vol.vol;
         fprintf('loaded vol %i \n', i);
     end
-
+    
+    % resize images in the volume
+    for i = 1:num_vols
+        [d1 d2 d3] = size(volumes{i});
+        for j = 1:d3
+            reduced_vols{i}(:,:,j) = imresize(volumes{i}(:,:,j), 0.5);
+        end
+    end
+    
     % Flatten each volume
     for i = 1:num_vols
-       flat_vols{i} = flatten_vol(volumes{i},0); 
+       flat_vols{i} = flatten_vol(reduced_vols{i},0); 
        fprintf('flattened vol %i \n', i);
     end
     disp('Done with flattening the volumes');
-
+    
+    % Filter using non-local Means filter
+    if (strcmp(lower(method),'lbp'))
+        options.kernelratio = 6;
+        options.windowratio = 6;
+        for i = 1:num_vols
+            [d1 d2 d3] = size(flat_vols{i});
+            for ii = 1:d3
+                I = (flat_vols{i}(:,:,ii));
+                max_I = max(max(I));
+                I = I / max_I;
+                J = NLMF(I,options);
+                flat_vols{i}(:,:,ii) = J*max_I;
+            end
+        end
+        disp('Done with NL means filtering images');
+    end
+    
     % Crop the volumes
     [cropped_vols] = crop_vols(flat_vols);
     disp('Done with cropping the volumes');
 
-    % Now vectorise frames and form X matrix
-    n = 1;
-    for i = 1:num_vols
-       [d1 d2 d3] = size(cropped_vols{i});
-       for j = 1:d3
-           img = imresize(cropped_vols{i}(:,:,j), 0.5);
-           X(:,n) = img(:);
-           n = n+1;
-       end
+    switch lower(method)
+        % We are dealing with intensity
+        case 'intensity'
+            % Now vectorise frames and form X matrix
+            n = 1;
+            for i = 1:num_vols
+               [d1 d2 d3] = size(cropped_vols{i});
+               for j = 1:d3
+                   %img = imresize(cropped_vols{i}(:,:,j), 0.5);
+                   img = cropped_vols{i}(:,:,j);
+                   X(:,n) = img(:);
+                   n = n+1;
+               end
+            end
+        % We are dealing with the texture
+        case 'lbp'
+            n = 1;
+            for i = 1:length(volumes)
+               [d1 d2 d3] = size(cropped_vols{i});
+               for j = 1:d3
+                  img = cropped_vols{i}(:,:,j);
+                  J = lbp(img, 2, 16, 0, 'h');
+                  X(:,n) = J(:);
+                  n = n+1;
+               end
+            end
+        % None of the above
+        otherwise
+            disp('not the right method for pre-processing');
+            X = 0;
+            return;
     end
+    
     disp('Done with forming the X matrix');
 
-end
--- run_lbp.m
+++ run_lbp.m
@@ -1,76 +1,50 @@
-% Before running, make sure the data is available in the data folder
-% The DME volumes are expected to be in /data/DME folder.
-% The normal volumes are expected to be in /data/normal folder.
-
 clc; clear all; close all;
+
+
+%% Some parameters for the algorithm
+
 dataset = 'SERI';
-
-%% Initalize - some settings and variables to run the algorithm
-
-do_plot = 0;
-% should we do preprocessing to arrive at X matrix. 
-% if not, we use the saved X - X_normal.mat and X_patient.mat
-do_preprocess = 0;
-% should we use the saved X values - X_normal.mat and X_patient.mat?
-saved_results = 1;
-% Varies based on the dataset
 num_vols = 16;
+pca_k = 800;
 frames_per_vol = 128; % for SERI
 total_frames = num_vols*frames_per_vol;
-% K for PCA dimensions
-pca_k = 1000;
-% Cross validation using volumens 13-15
-% try different K values for GMM to decide which is best
-GMM_Ks = [2 3 5 8 10 12 15];
-% Threshold for the mahalnobis distance to separate the outliers.
-% the more we set, the lesser outliers (diseased) frames.
-mahal_thresh = chi2inv(0.95, pca_k); % for our dataset
-% Number of frames to train with. Used for cross validation
 train_frames = 12*frames_per_vol; 
+mahal_thresh = chi2inv(0.95, pca_k);
+do_preprocess = 0;
 
+%% Extract LBP features and form the X matrix
 
-%% Read data and pre-process them for flattening
 
 if (do_preprocess)
-    [X,X_fvols] = preprocess('data/normal/normal',num_vols, dataset, 'intensity');
-    [Y,Y_fvols] = preprocess('data/DME/patient',num_vols, dataset, 'intensity');
-end
-
-% save('X_patient_1.mat','Y','-v7.3');
-% save('X_normal_1.mat','X','-v7.3');
-
-%% Do PCA to lower dimension
-
-if (saved_results)
-    X = load('X_normal_1');
+    [X f_vols] = preprocess('data/normal/normal',num_vols, dataset, 'lbp');
+else
+    X = load('X_filtered_lbp');
     X = X.X;
 end
-%X = X';
-[X_res, x_mapping] = do_pca(X,pca_k);
-disp('Done with loading X - normal cases');
 
-% Choose K based on this ratio. Above 0.9 is good
-%sum(x_mapping.lambda(1:pca_k))/sum(x_mapping.lambda);
-%plot3(X_res(:,1),X_res(:,2),X_res(:,3),'rx');
+[X_res, x_mapping] = do_pca(X',pca_k);
+disp('Done with PCA of X');
+fprintf('Variance retained after PCA is: %f \n', ...
+         sum(x_mapping.lambda(1:pca_k))/sum(x_mapping.lambda));
 
-if (saved_results)
-    Y = load('X_patient_1');
-    Y = Y.Y; 
+if (do_preprocess)
+    Y = preprocess('data/DME/patient',num_vols, dataset, 'lbp');
+else
+    Y = load('Y_filtered_lbp');
+    Y = Y.Y;
 end
-%Y = Y';
-[Y_res, y_mapping] = do_pca(Y,pca_k);
-disp('done with loading Y - DME cases');
+[Y_res, y_mapping] = do_pca(Y',pca_k);
+disp('Done with PCA of Y');
+fprintf('Variance retained after PCA is: %f \n', ...
+         sum(x_mapping.lambda(1:pca_k))/sum(x_mapping.lambda));
 
-fprintf('Variance retained after PCA is: %f \n', sum(x_mapping.lambda(1:pca_k))/sum(x_mapping.lambda));
-
-
-%% Now fit a GMM with the data
-% Cross validate for different values of GMM K values
-% and choose which is best 
+%% Fit GMM and do cross validation to arrive at GMM K value
 
 clc;
-GMM_Ks = [2 3 5 8 10 12];
+clear total_diseased;
+GMM_Ks = [2 3 5 8 10 12 15];
 options = statset('Display','off','MaxIter',1000);
+
 
 for ii = 1:length(GMM_Ks)
     gmm_k = GMM_Ks(ii);
@@ -80,7 +54,7 @@ for ii = 1:length(GMM_Ks)
                             'Regularize',0.001, ...
                             'Start', 'randSample', ...
                             'CovType', 'diagonal', ...
-                            'Replicates', 5, ...
+                            'Replicates', 3, ...
                             'Start', 'randSample');
 
     outliers = {};
@@ -91,7 +65,7 @@ for ii = 1:length(GMM_Ks)
     for i = train_frames+1:total_frames
        %frame_dists(n) = norm(X_res(i,:) - gmm_means);
        [x_outliers(n)] = check_outlier(...
-                                  obj, X_res(i,:), gmm_k, mahal_thresh);
+                                  obj, Y_res(i,:), gmm_k, mahal_thresh);
        n = n+1;
        frame_no = frame_no+1;
 
@@ -127,12 +101,13 @@ end
 % Choose the best K value for GMM. 
 % We choose the K that returns the least number of diseased frames
 % If more than 2 Ks are best, chose the  min.
-best_gmm_k = min(GMM_Ks(find(total_diseased == min(total_diseased))));
+best_gmm_k = max(GMM_Ks(find(total_diseased == min(total_diseased))));
 
 
 %% Classification of diseased volumes
 % To run this step, run the GMM fit step for all volumes.
 
+%mahal_thresh = 1000;
 clc;
 outliers = {};
 y_outliers = [];
@@ -140,6 +115,7 @@ n = 1; % count number of outliers
 frame_no = 1; % frame number in the volume
 vol = 1;
 options = statset('Display','final','MaxIter',1000);
+best_gmm_k = 3;
 
 % final model with the best K value for GMM
 % train with all training data
@@ -153,7 +129,7 @@ obj = gmdistribution.fit(X_res,best_gmm_k, ...
 
 for i = 1:total_frames
     [y_outliers(n)] = check_outlier(...
-                            obj, Y_res(i,:),gmm_k, mahal_thresh);
+                            obj, Y_res(i,:),best_gmm_k, mahal_thresh);
     n = n+1;
     frame_no = frame_no+1;
     % reset for next volume
@@ -178,4 +154,5 @@ for i = 1:length(outliers)
 end
 fprintf('Diseased volumes are: %d, total diseased frames are: %d \n',...
         diseased_vols, diseased_frames);
-
+    
+    
